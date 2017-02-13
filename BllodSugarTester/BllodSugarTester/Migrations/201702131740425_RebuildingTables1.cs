f10a9858151db2e12eef892500cabcdda7b4d318
Nik Dries <ninja1704@hotmail.com>
2017-02-13 11:50:36 -0600

    fixing tables

---
+++ BllodSugarTester/BllodSugarTester/Migrations/201702131740425_RebuildingTables1.cs
@@ -0,0 +1,43 @@
+namespace BllodSugarTester.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class RebuildingTables1 : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+               "dbo.AspNetUsers",
+               c => new
+               {
+                   Id = c.Int(nullable: false),
+                   UserName = c.String(nullable: false, maxLength: 256),
+                   FirstName = c.String(),
+                   LastName = c.String(),
+                   Email = c.String(maxLength: 256),
+                   EmailConfirmed = c.Boolean(nullable: false),
+                   PasswordHash = c.String(),
+                   SecurityStamp = c.String(),
+                   DoctorId = c.String(),
+                   BSUpperNumber = c.Int(),
+                   BSLowerNumber = c.Int(),
+                   PhoneNumberConfirmed = c.String(),
+                   TwoFactorEnabled = c.String(),
+                   LockoutEndDateUtc = c.String(),
+                   LockoutEnabled = c.String(),
+                   AccessFailedCount = c.String(),
+               })
+            .PrimaryKey(t => t.Id)
+            .Index(t => t.UserName, unique: true, name: "UserNameIndex")
+
+            ;
+        }
+
+        public override void Down()
+        {
+            DropIndex("dbo.AspNetUsers", "UserNameIndex");
+            DropTable("dbo.AspNetUsers");
+        }
+    }
+}
