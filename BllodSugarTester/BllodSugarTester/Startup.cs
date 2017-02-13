9024d6f98043d4996c22e2e728278922cc6c5d18
Nik Dries <ninja1704@hotmail.com>
2017-02-13 07:49:16 -0600

    initial commit

---
+++ BllodSugarTester/BllodSugarTester/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BllodSugarTester.Startup))]
+namespace BllodSugarTester
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
